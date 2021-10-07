4c77c689a931a4109ba18d676f46aa5935662f77
Rayan <rayan.bernardes@upflow.me>
2021-10-07 15:31:14 -0300

    feat: Car as Marker
    Marker is now a car image;

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import whiteCar64 from '../img/whiteCar.js'
 
 class Map extends React.Component {
 
@@ -7,6 +8,11 @@ class Map extends React.Component {
 
         super(props);
         this.state = {
+            iconSettings: {
+                url: whiteCar64,
+                scaledSize: (30, 30),
+                anchor: { x: 15, y: 15 }
+            }
         }
 
     }
@@ -25,8 +31,18 @@ class Map extends React.Component {
                             mapContainerStyle={{ width: '100%', height: '100%' }}
                             center={this.props.mapData[0]}
                             zoom={15}
+                            onLoad={map => {
+                                const iconSize = new window.google.maps.Size(30, 30);
+                                this.setState({
+                                    iconSettings: {
+                                        ...this.state.iconSettings,
+                                        scaledSize: iconSize
+                                    }
+                                })
+                            }}
                         >
                             <Marker
+                                icon={this.state.iconSettings}
                                 position={this.props.mapData[0]}
                             />
                         </GoogleMap>
